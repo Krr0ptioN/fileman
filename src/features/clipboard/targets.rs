4b5b7e8dffed7792b94ff41f587271f2660160b5
Mardin Nasouti <info@mardin.cc>
2026-07-23 14:04:58 +0300

    perf: reuse the empty clipboard target set

--- src/features/clipboard/targets.rs
+++ src/features/clipboard/targets.rs
@@ -1,13 +1,19 @@
-use std::{collections::HashSet, path::PathBuf, sync::Arc};
+use std::{
+    collections::HashSet,
+    path::PathBuf,
+    sync::{Arc, OnceLock},
+};
 
 use gpui::App;
 
 use super::{ClipboardKind, ClipboardState};
 
+static EMPTY_TARGET_PATHS: OnceLock<Arc<HashSet<PathBuf>>> = OnceLock::new();
+
 pub fn target_paths(cx: &App, kind: ClipboardKind) -> Arc<HashSet<PathBuf>> {
     let clipboard = cx.global::<ClipboardState>();
     match clipboard.op.as_ref() {
         Some(clipboard) if clipboard.kind == kind => clipboard.paths.clone(),
-        _ => Arc::new(HashSet::new()),
+        _ => Arc::clone(EMPTY_TARGET_PATHS.get_or_init(|| Arc::new(HashSet::new()))),
     }
 }
